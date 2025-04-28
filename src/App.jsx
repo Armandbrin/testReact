36161e49a44e49c82188d7593309c6dbfa471af5
Armand BRIN <ABRIN@NAHEULBEUK.ACAD>
2025-04-28 15:21:25 +0200

    exo4 fini

--- src/App.jsx
+++ src/App.jsx
@@ -35,24 +35,27 @@ const Footer = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = { name:"Fundamentals of React", exercices:10}
-  const part2 = { name:"Using props to pass data", exercices:7}
-  const part3 = { name:"State of a component", exercices:14}
+  const parts = [
+    { name: "Fundamentals of React", exercices: 10 },
+    { name: "Using props to pass data", exercices: 7 },
+    { name: "State of a component", exercices: 14 },
+  ];
+
   return (
     <>
       <Header course={course} />
-      <Content 
-      part1={part1.name}
-      part2={part2.name}
-      part3={part3.name}
-      exercices1={part1.exercices}
-      exercices2={part2.exercices}
-      exercices3={part3.exercices}
+      <Content
+        part1={parts[0].name}
+        part2={parts[1].name}
+        part3={parts[2].name}
+        exercices1={parts[0].exercices}
+        exercices2={parts[1].exercices}
+        exercices3={parts[2].exercices}
       />
       <Footer
-        exercices1={part1.exercices}
-        exercices2={part2.exercices}
-        exercices3={part3.exercices}
+        exercices1={parts[0].exercices}
+        exercices2={parts[1].exercices}
+        exercices3={parts[2].exercices}
       />
     </>
   );
